28de759cf3da9f642fe0f9bd835d159673da6845
Dhruval <dhruvalbhuva2000@gmail.com>
2021-06-13 10:23:13 +0530

    update

--- srs/app.js
+++ srs/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
-const port = process.env.PORT || 7000; // if hosted then user port num otherwise for local host 3000
+const port = process.env.PORT || 8000; // if hosted then user port num otherwise for local host 3000
 
 // paths
 const staticPath = path.join(__dirname, "../public");
